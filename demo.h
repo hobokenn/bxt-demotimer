5dc23150b7ee1525437db230e1feb10dbc074f33
Marat Mustafin <nopenope8@gmail.com>
2023-04-25 02:12:46 +0300

    Add project files.

---
+++ demo.h
@@ -0,0 +1,98 @@
+#pragma once
+
+void getOriginalBytes(std::vector<uint8_t> &bytes);
+void TEA_Decrypt(uint32_t v[2], const uint32_t k[4]);
+void getLastBxtTime(const std::vector<uint8_t> &bytes);
+inline void waitForEnter()
+{
+	std::cout << "Press ENTER to continue...";
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+struct demoheader_t
+{
+	char szFileStamp[6];
+	int nDemoProtocol;
+	int nNetProtocol;
+	char szMapName[260];
+	char szDllDir[260];
+	uint32_t mapCRC;
+	int nDirectoryOffset;
+};
+
+struct demoentry_t
+{
+	int nEntryType;
+	char szDescription[64];
+	int nFlags;
+	int nCDTrack;
+	float fTrackTime;
+	int nFrames;
+	int nOffset;
+	int nFileLength;
+};
+
+struct demodirectory_t
+{
+	int nEntries;
+	demoentry_t *p_rgEntries;
+};
+
+enum class DemoFrameType : uint8_t {
+	DEMO_START = 2,
+	CONSOLE_COMMAND = 3,
+	CLIENT_DATA = 4,
+	NEXT_SECTION = 5,
+	EVENT = 6,
+	WEAPON_ANIM = 7,
+	SOUND = 8,
+	DEMO_BUFFER = 9
+};
+
+struct demo_command_t
+{
+	DemoFrameType cmd;
+	float time;
+	int frame;
+};
+
+struct BxtData
+{
+	char header[7];
+	char data[56];
+};
+
+enum class RuntimeDataType : uint8_t {
+	VERSION_INFO = 1,
+	CVAR_VALUES,
+	TIME,
+	BOUND_COMMAND,
+	ALIAS_EXPANSION,
+	SCRIPT_EXECUTION,
+	COMMAND_EXECUTION,
+	GAME_END_MARKER,
+	LOADED_MODULES,
+	CUSTOM_TRIGGER_COMMAND,
+	EDICTS,
+	PLAYERHEALTH,
+	SPLIT_MARKER,
+};
+
+struct Time {
+	uint32_t hours;
+	uint8_t minutes;
+	uint8_t seconds;
+	double remainder;
+};
+
+const uint32_t KEY[4] = { 0x1337FACE, 0x12345678, 0xDEADBEEF, 0xFEEDABCD };
+constexpr uint8_t ESCAPE_BYTE = 0xFF;
+constexpr uint8_t FILL_BYTE = 0xFE;
+
+const std::unordered_map<uint8_t, uint8_t> ESCAPE_CHARACTERS = {
+	{ 0x01, 0x00 },
+	{ 0x02 , '"' },
+	{ 0x03, '\n' },
+	{ 0x04, ';' },
+	{ 0xFF, 0xFF },
+};
---
+++ main.cpp
@@ -0,0 +1,243 @@
+#include <cstdint>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <cassert>
+#include "demo.h"
+
+int main(int argc, char* argv[])
+{
+	if (argc == 1) {
+		std::cout << "bxt-demotimer.exe <demoname>\n";
+		waitForEnter();
+		return 1;
+	}
+	
+	std::ifstream input(argv[1], std::ios::binary | std::ios::in);
+	
+	if (!input.is_open()) {
+		std::cout << "couldn't open the file.\n";
+		waitForEnter();
+		return 1;
+	}
+
+	demoheader_t demoHeader;
+	input.read(demoHeader.szFileStamp, sizeof(demoHeader.szFileStamp));
+	if (std::strncmp(demoHeader.szFileStamp, "HLDEMO", 6)) {
+		std::cout << "not a goldsrc demo.\n";
+		waitForEnter();
+		return 1;
+	}
+
+	input.seekg(offsetof(demoheader_t, nNetProtocol), std::ios_base::beg);
+	input.read(reinterpret_cast<char*>(&demoHeader.nNetProtocol), sizeof(int));
+	
+	if (demoHeader.nNetProtocol < 46) {
+		std::cout << "unsupported net protocol, demo was probably recorded on the original WON version.\n";
+		waitForEnter();
+		return 1;
+	}
+
+	input.seekg(offsetof(demoheader_t, nDirectoryOffset), std::ios_base::beg);
+	input.read(reinterpret_cast<char*>(&demoHeader.nDirectoryOffset), sizeof(int));
+
+	input.seekg(demoHeader.nDirectoryOffset, std::ios_base::beg);
+
+	demodirectory_t directoryEntry;
+	input.read(reinterpret_cast<char*>(&directoryEntry.nEntries), sizeof(int));
+	std::cout << "parsing the demo file...\n";
+
+	// TODO: rewrite without iterating over directory entries?
+	// just seek whatever bytes from the end and read
+	// number of frames and offset for PLAYBACK
+	for (int i = 0; i < directoryEntry.nEntries; ++i) {
+		demoentry_t demoEntry;	
+		input.seekg(offsetof(demoentry_t, nFrames), std::ios_base::cur);
+		input.read(reinterpret_cast<char*>(&demoEntry.nFrames), sizeof(int));
+		input.read(reinterpret_cast<char*>(&demoEntry.nOffset), sizeof(int));
+
+		input.seekg(demoEntry.nOffset, std::ios_base::beg);
+
+		demo_command_t demoFrame;
+		input.read(reinterpret_cast<char*>(&demoFrame.cmd), sizeof(uint8_t));
+		int sampleCount = 0, bufferSize = 0, msglen = 0;
+		BxtData bxtData; std::vector<uint8_t> packedBxtBytes; static bool isBxtDemo;
+
+		do {
+			switch (demoFrame.cmd) {
+			case DemoFrameType::DEMO_START:
+				input.seekg(8, std::ios_base::cur);
+
+				input.read(reinterpret_cast<char*>(&demoFrame.cmd), sizeof(uint8_t));
+				break;
+			case DemoFrameType::CONSOLE_COMMAND:
+				input.seekg(8, std::ios_base::cur);
+				// handle //BXTD0 here
+				input.read(bxtData.header, 7);
+				if (!std::strncmp(bxtData.header, "//BXTD0", 7)) {
+					isBxtDemo = true;
+					input.read(bxtData.data, 56);
+					for (int i = 0; i < sizeof(bxtData.data); ++i) {
+						if (bxtData.data[i] == '\0') break;
+						// is std::vector the best container here? if i want to parse all of
+						// bxt data later and not just last bxt time i need to think of something else
+						packedBxtBytes.emplace_back(bxtData.data[i]);
+					}
+					// null terminator
+					input.seekg(1, std::ios_base::cur);
+				} else {
+					input.seekg(57, std::ios_base::cur);
+				}
+
+				input.read(reinterpret_cast<char*>(&demoFrame.cmd), sizeof(uint8_t));
+				break;
+			case DemoFrameType::CLIENT_DATA:
+				input.seekg(8, std::ios_base::cur);
+				input.seekg(32, std::ios_base::cur);
+
+				input.read(reinterpret_cast<char*>(&demoFrame.cmd), sizeof(uint8_t));
+				break;
+			case DemoFrameType::NEXT_SECTION:
+				break;
+			case DemoFrameType::EVENT:
+				input.seekg(8, std::ios_base::cur);
+				input.seekg(84, std::ios_base::cur);
+
+				input.read(reinterpret_cast<char*>(&demoFrame.cmd), sizeof(uint8_t));
+				break;
+			case DemoFrameType::WEAPON_ANIM:
+				input.seekg(8, std::ios_base::cur);
+				input.seekg(8, std::ios_base::cur);
+
+				input.read(reinterpret_cast<char*>(&demoFrame.cmd), sizeof(uint8_t));
+				break;
+			case DemoFrameType::SOUND:
+				input.seekg(8, std::ios_base::cur);
+				input.seekg(4, std::ios_base::cur);
+				input.read(reinterpret_cast<char*>(&sampleCount), sizeof(int));
+				input.seekg(sampleCount, std::ios_base::cur);
+				input.seekg(16, std::ios_base::cur);
+
+				input.read(reinterpret_cast<char*>(&demoFrame.cmd), sizeof(uint8_t));
+				break;
+			case DemoFrameType::DEMO_BUFFER:
+				input.seekg(8, std::ios_base::cur);
+				input.read(reinterpret_cast<char*>(&bufferSize), sizeof(int));
+				input.seekg(bufferSize, std::ios_base::cur);
+
+				input.read(reinterpret_cast<char*>(&demoFrame.cmd), sizeof(uint8_t));
+				break;
+			default:
+				input.seekg(8, std::ios_base::cur);
+				input.seekg(436, std::ios_base::cur);
+				input.seekg(28, std::ios_base::cur);
+				input.read(reinterpret_cast<char*>(&msglen), sizeof(int));
+				input.seekg(msglen, std::ios_base::cur);
+
+				input.read(reinterpret_cast<char*>(&demoFrame.cmd), sizeof(uint8_t));
+				break;
+			}
+		} while (demoFrame.cmd != DemoFrameType::NEXT_SECTION);
+
+		if (i == 0) {
+			input.seekg(demoHeader.nDirectoryOffset, std::ios_base::beg);
+			input.seekg(4, std::ios_base::cur);
+			input.seekg(sizeof(demoentry_t), std::ios_base::cur);
+		} else {
+			std::cout << "reached the end of the demo\n";
+
+			if (!isBxtDemo) {
+				input.read(reinterpret_cast<char*>(&demoFrame.time), sizeof(float));
+				input.read(reinterpret_cast<char*>(&demoFrame.frame), sizeof(int));
+				std::cout << "this demo was not recorded with BXT, however here's some information the game provides itself when you stop a demo:\n";
+				std::cout << "Recording time: " << std::fixed << std::setprecision(2) << demoFrame.time << '\n';
+				std::cout << "Host frames: " << demoFrame.frame << '\n';
+				break;
+			}
+
+			getOriginalBytes(packedBxtBytes);
+
+			for (size_t i = 0; i < packedBxtBytes.size(); i += 8)
+				TEA_Decrypt(reinterpret_cast<uint32_t*>(packedBxtBytes.data() + i), KEY);
+
+			assert(!packedBxtBytes.empty());
+			while (packedBxtBytes.back() == FILL_BYTE)
+				packedBxtBytes.pop_back();
+
+			getLastBxtTime(packedBxtBytes);
+
+			break;
+		}
+	}
+	waitForEnter();
+	return 0;
+}
+
+void TEA_Decrypt(uint32_t v[2], const uint32_t k[4]) {
+	uint32_t v0 = v[0], v1 = v[1], sum = 0xC6EF3720, i;  /* set up; sum is (delta << 5) & 0xFFFFFFFF */
+	uint32_t delta = 0x9E3779B9;                     /* a key schedule constant */
+	uint32_t k0 = k[0], k1 = k[1], k2 = k[2], k3 = k[3];   /* cache key */
+	for (i = 0; i < 32; i++) {                         /* basic cycle start */
+		v1 -= ((v0 << 4) + k2) ^ (v0 + sum) ^ ((v0 >> 5) + k3);
+		v0 -= ((v1 << 4) + k0) ^ (v1 + sum) ^ ((v1 >> 5) + k1);
+		sum -= delta;
+	}                                              /* end cycle */
+	v[0] = v0; v[1] = v1;
+}
+
+void getOriginalBytes(std::vector<uint8_t> &bytes)
+{
+	for (auto it = bytes.begin(); it != bytes.end(); ++it) {
+
+		if (std::next(it, 1) == bytes.end())
+			break;
+
+		uint8_t nextChar = *std::next(it, 1);
+		auto escaped = ESCAPE_CHARACTERS.find(nextChar);
+		if (it[0] == ESCAPE_BYTE && nextChar == escaped->first) {
+			// is this slow? probably
+			it = bytes.erase(it, std::next(it, 2));
+			it = bytes.emplace(it, escaped->second);
+		}
+	}
+}
+
+void getLastBxtTime(const std::vector<uint8_t> &bytes)
+{
+	// unless BXT code changes and time is not the last thing written
+	// to a demo on CL_Stop_f(), this code should work https://cdn.7tv.app/emote/6268904f4f54759b7184fa72/4x.webp
+	// until then this is the laziest solution i could think of.
+	char buf[15];
+	std::memcpy(&buf, &bytes.back() - 14, 15);
+	std::string str = std::string(buf, 15);
+	
+	std::istringstream ss(str, std::ios::binary);
+	Time time; char type;
+	ss.read(&type, 1);
+	assert(type == '\3' && "what the hell is going on?");
+
+	ss.read(reinterpret_cast<char*>(&time.hours), sizeof(uint32_t));
+	ss.read(reinterpret_cast<char*>(&time.minutes), sizeof(uint8_t));
+	ss.read(reinterpret_cast<char*>(&time.seconds), sizeof(uint8_t));
+	ss.read(reinterpret_cast<char*>(&time.remainder), sizeof(double));
+
+	// pretty ugly
+	if (time.hours == 0 && time.minutes == '\0' && time.seconds == '\0')
+		std::cout << std::fixed << std::setprecision(3)
+		<< "bxt demo time: " <<  time.remainder << '\n';
+	else if (time.hours == 0 && time.minutes == '\0')
+		std::cout << std::fixed << std::setprecision(3)
+		<< "bxt demo time: " << time.seconds + time.remainder << '\n';
+	else if (time.hours == 0)
+		std::cout << std::fixed << std::setprecision(3)
+		<< "bxt demo time: " << static_cast<int>(time.minutes) << ":"
+		<< time.seconds + time.remainder << '\n';
+	else
+		std::cout << std::fixed << std::setprecision(3)
+		<< "bxt demo time: " << time.hours << ":" << static_cast<int>(time.minutes)
+		<< ":" << time.seconds + time.remainder << '\n';
+}
